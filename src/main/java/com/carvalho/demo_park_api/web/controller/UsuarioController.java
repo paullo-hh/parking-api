01301b4d4a5eb3e2b2e86746a5f94747af5f5cf8
Paulo H. Sousa <contacthenry95@gmail.com>
2025-07-27 01:09:27 -0300

    fix: corrige formatação no import de `HttpStatus` em `UsuarioController`

--- src/main/java/com/carvalho/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/carvalho/demo_park_api/web/controller/UsuarioController.java
@@ -3,7 +3,7 @@ package com.carvalho.demo_park_api.web.controller;
 import com.carvalho.demo_park_api.entity.Usuario;
 import com.carvalho.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org .springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
