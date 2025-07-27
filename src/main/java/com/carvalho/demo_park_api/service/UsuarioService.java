9128e94751503e17970aa35db768397f390f63c9
Paulo H. Sousa <contacthenry95@gmail.com>
2025-07-27 00:56:42 -0300

    feat: adiciona endpoint de criação de usuário com persistência no banco

--- src/main/java/com/carvalho/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/carvalho/demo_park_api/service/UsuarioService.java
@@ -1,13 +1,18 @@
 package com.carvalho.demo_park_api.service;
 
+import com.carvalho.demo_park_api.entity.Usuario;
 import com.carvalho.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
   private final UsuarioRepository usuarioRepository;
 
-
+  @Transactional
+  public Usuario salvar(Usuario usuario) {
+    return usuarioRepository.save(usuario);
+  }
 }
