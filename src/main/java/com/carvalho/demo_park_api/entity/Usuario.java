5ed993f3a13cdb9a7d527f16cbe079520ac305f5
Paulo H. Sousa <contacthenry95@gmail.com>
2025-07-27 00:39:38 -0300

    fix: remove atributo único desnecessário da coluna `password` em `Usuario`

--- src/main/java/com/carvalho/demo_park_api/entity/Usuario.java
+++ src/main/java/com/carvalho/demo_park_api/entity/Usuario.java
@@ -24,7 +24,7 @@ public class Usuario implements Serializable {
   @Column(name = "username", nullable = false, unique = true, length = 100)
   private String username;
 
-  @Column(name = "password", nullable = false, unique = false, length = 200 )
+  @Column(name = "password", nullable = false, length = 200 )
   private String password;
 
   @Enumerated(EnumType.STRING)
