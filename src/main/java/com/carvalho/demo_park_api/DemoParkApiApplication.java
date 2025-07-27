a26cb6a27aba91b7e6ca3cb55598cb20b816dbfe
Paulo H. Sousa <contacthenry95@gmail.com>
2025-07-27 15:05:53 -0300

    feat: adiciona suporte a variáveis de ambiente usando `dotenv-java` e ajusta configuração de conexão com o banco

--- src/main/java/com/carvalho/demo_park_api/DemoParkApiApplication.java
+++ src/main/java/com/carvalho/demo_park_api/DemoParkApiApplication.java
@@ -1,5 +1,6 @@
 package com.carvalho.demo_park_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoParkApiApplication {
 
 	public static void main(String[] args) {
+		loadEnv();
 		SpringApplication.run(DemoParkApiApplication.class, args);
 	}
 
+	private static void loadEnv() {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach ( entry  -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+	}
 }
