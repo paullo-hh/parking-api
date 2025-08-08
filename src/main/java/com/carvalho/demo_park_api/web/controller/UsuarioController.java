8b0fdfdb8578514dcd3652b578497893b018c9ed
Paulo H. Sousa <contacthenry95@gmail.com>
2025-08-08 10:07:34 -0300

    feat: adiciona documentação OpenAPI para operações com usuários no `UsuarioController`

--- src/main/java/com/carvalho/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/carvalho/demo_park_api/web/controller/UsuarioController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Tag(
-    name = "Usuários",
+    name = "Usuarios",
     description = "Contém todos as operações relativas aos recursos para cadastro, edição e leitura de usuários."
 )
 @RestController
@@ -72,18 +72,96 @@ public class UsuarioController {
     return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
   }
 
+  @Operation(
+      summary="Recuperar um usuário pelo id.",
+      description = "Recurso para recuperar um usuário pelo id.",
+      responses = {
+          @ApiResponse(
+              responseCode = "200",
+              description = "Recurso recuperado com sucesso.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = UsuarioResponseDTO.class
+                  )
+              )
+          ),
+          @ApiResponse(
+              responseCode = "404",
+              description = "Recurso não encontrado.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = ErrorMessage.class
+                  )
+              )
+          )
+      }
+  )
   @GetMapping("/{id}")
   public ResponseEntity<UsuarioResponseDTO> getById(@PathVariable Long id) {
     Usuario user = usuarioService.buscarPorId(id);
     return ResponseEntity.ok(UsuarioMapper.toDto(user));
   }
 
+  @Operation(
+      summary="Atualizar senha.",
+      description = "Recurso para Atualizar senha.",
+      responses = {
+          @ApiResponse(
+              responseCode = "204",
+              description = "Senha atualizada com sucesso.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = Void.class
+                  )
+              )
+          ),
+          @ApiResponse(
+              responseCode = "400",
+              description = "Senha não confere.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = ErrorMessage.class
+                  )
+              )
+          ),
+          @ApiResponse(
+              responseCode = "404",
+              description = "Recurso não encontrado.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = ErrorMessage.class
+                  )
+              )
+          )
+      }
+  )
   @PatchMapping("/{id}")
   public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UsuarioSenhaDTO usuarioSenhaDTO) {
     Usuario user = usuarioService.editarSenha(id, usuarioSenhaDTO.getSenhaAtual(), usuarioSenhaDTO.getNovaSenha(), usuarioSenhaDTO.getConfirmaSenha());
     return ResponseEntity.noContent().build();
   }
 
+  @Operation(
+      summary="Recuperar todos os usuário.",
+      description = "Recurso para recuperar todos os usuário.",
+      responses = {
+          @ApiResponse(
+              responseCode = "200",
+              description = "Recurso recuperado com sucesso.",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(
+                      implementation = UsuarioResponseDTO.class
+                  )
+              )
+          )
+      }
+  )
   @GetMapping
   public ResponseEntity<List<UsuarioResponseDTO>> getAll() {
     List<Usuario> users = usuarioService.buscarTodos();
